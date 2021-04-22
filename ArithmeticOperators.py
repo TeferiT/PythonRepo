b3616b3ee534189bf1b9d0477914f3c459605b30
unknown <teferia202@gmail.com>
2021-04-22 14:49:12 -0400

    more operator to add

---
+++ ArithmeticOperators.py
@@ -0,0 +1,14 @@
+
+"""
+a=10
+b=20
+c=a+b
+print( "the total is:",c)"""
+mark=int(input("please enter mark:"))
+if mark>90:
+  grade="A"
+elif mark>80:
+  grade="B"
+else:
+  grade="less"
+print(grade)
